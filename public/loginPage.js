287d909fc02984847fc0d8bbfcd4ea2f4cbf6ad4
Alexander Burachkov <zicio@yandex.ru>
2021-03-31 22:57:02 +0400

    loginPage.js Updated

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,7 +1,21 @@
 'use strict'
 const userForm = new UserForm();
 userForm.loginFormCallback = (data) => {
-    const fn = () => console.log(data);
+    const fn = () => {
+        let error = '';
+        if (data.login.length === 0) {
+            error += 'Поле "Логин" обязательно для заполнения.';
+        }
+        if (data.password.length === 0) {
+            error += ' Поле "Пароль" обязательно для заполнения.';
+        }
+        
+        if (error !== '') {
+            console.log(error);
+            userForm.loginErrorMessageBox.style.display = 'block';
+            userForm.loginErrorMessageBox.innerHTML = error;
+            return;
+        }
+    }
     ApiConnector.login(data, fn);
 }
-
